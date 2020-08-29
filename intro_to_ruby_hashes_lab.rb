ab2b093b80a060c2c59289bddf28cf2dc282ccd0
Lisa Walters <campi_l@fortlewis.edu>
2020-08-29 20:53:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,8 +9,8 @@ end
 
 def monopoly
 
-	monopoly = {railroads: {
-:pieces => 4,
+	monopoly = {railroads: =>
+{:pieces => 4,
 :rent_in_dollars => {
   :one_piece_owned => 25,
   :two_pieces_owned => 50,
