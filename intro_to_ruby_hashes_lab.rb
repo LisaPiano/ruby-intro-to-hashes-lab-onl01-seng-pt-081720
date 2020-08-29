94d30467a70cf098d0d813318d09e9691febec8c
Lisa Walters <campi_l@fortlewis.edu>
2020-08-29 21:56:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -78,11 +78,11 @@ def monopoly_with_fourth_tier
       },
       :b_and_o_railroad =>
       {
-        "mortgage_value" => "400"
+        "mortgage_value" => "$400"
       },
       :shortline =>
       {
-        "mortgage_value" => "800"
+        "mortgage_value" => "$800"
       }
 
     },#end names
