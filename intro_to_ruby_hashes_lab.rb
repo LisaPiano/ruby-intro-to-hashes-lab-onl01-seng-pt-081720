1dca82d177a0b1b1b6c36d9caa1b9dfc80f084ac
Lisa Walters <campi_l@fortlewis.edu>
2020-08-29 21:29:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -66,19 +66,19 @@ def monopoly_with_fourth_tier
     :pieces => 4,
     :names =>
     {
-      :reading_railroad => 
+      :reading_railroad =>
       {
         "mortgage_value" => $100
       },
-      :pennsylvania_railroad => 
+      :pennsylvania_railroad =>
       {
         "mortgage_value" => $200
       },
-      :b_and_o_railroad => 
+      :b_and_o_railroad =>
       {
         :"mortgage-value" => $400
       },
-      :shortline => 
+      :shortline =>
       {
         :"mortgage-value" => $800
       }
